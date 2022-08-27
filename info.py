81bc590375a70fdb9ea5687a2bf78ff9a0729e8f
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-08-27 22:21:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,4 +14,7 @@ def is_enabled(value, default):
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
-SESSION = environ.get('SESSION', 'Media_search')
+SESSION = environ.get('SESSION', 'Media_search') 
+
+# Admins
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
