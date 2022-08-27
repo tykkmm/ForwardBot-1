a7a890ea23f9f61a638b72b895343d159921f56e
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-08-27 22:16:05 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,17 @@
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+# Bot information
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
+SESSION = environ.get('SESSION', 'Media_search')
