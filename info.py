d0bca59fef6ae4930b80643779923021fe4bfcee
tykkmm <tykhanew45@gmail.com>
2024-07-22 18:55:00 +0500

    Update info.py

--- info.py
+++ info.py
@@ -11,10 +11,10 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
-SESSION = environ.get('SESSION', 'Media_search') 
+API_ID = int(environ['29426486'])
+API_HASH = environ['d71ad4ec048ab41677a1a439b21ff0c9']
+BOT_TOKEN = environ['7355006985:AAEY8ijg4CP-8GgcliRGJja87Tby78QT7To']
+SESSION = environ.get('forwarderofty', 'Media_search') 
 
 # Admins
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
